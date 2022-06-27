e2730316d9162eb02bb9a5b74695eeee88c8c273
Volodymyr Romanenko <romanonebe@gmail.com>
2022-06-27 21:34:16 +0300

    Update fetchCountries.js

--- src/API/fetchCountries.js
+++ src/API/fetchCountries.js
@@ -5,7 +5,7 @@ import Notiflix from 'notiflix';
 export function fetchCountries(name) {
   const options = '?fields=name,capital,flags,languages,population';
   const BASE_URL = 'https://restcountries.com/v2/name/';
-  const itemToFind = BASE_URL + name + options;
+  const itemToFind = `${BASE_URL}${name}${options}`;
   return fetch(itemToFind).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
